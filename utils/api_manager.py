c3a80b1ba1efc7654d35e8258fc8ccd6f4be7a53
Kottaavec2t <mathieu2008blanche@gmail.com>
2026-06-08 13:26:04 +0200

    refactor: add main function for get and post

--- utils/api_manager.py
+++ utils/api_manager.py
@@ -1,21 +1,27 @@
 import requests
 
-def get_api_data_json(url: str, headers: dict = None, params: dict = None) -> dict | None:
+def get_api_data(url: str, headers: dict = None, params: dict = None):
     try:
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
-        return response.json()
+        return response
     
     except requests.exceptions.HTTPError as http_e:
         print(f"HTTP error occurred: {http_e}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def get_api_data_image(url: str, headers: dict = None):
+def get_api_data_json(url: str, headers: dict = None, params: dict = None):
+    return get_api_data(url, headers=headers, params=params).json()
+
+def get_api_data_image(url: str, headers: dict = None, params: dict = None):
+    return get_api_data(url, headers=headers, params=params).content
+
+def post_api_data(url: str, json: dict = None):
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.post(url, json=json)
         response.raise_for_status()
-        return response.content
+        return response
     
     except requests.exceptions.HTTPError as http_e:
         print(f"HTTP error occurred: {http_e}")
@@ -23,15 +29,4 @@ def get_api_data_image(url: str, headers: dict = None):
         print(f"An error occurred: {e}")
 
 def post_api_data_json(url: str, json: dict = None):
-    try:
-        response = requests.post(url, json=json)
-        response.raise_for_status()
-        return response.json()
-    
-    except requests.exceptions.HTTPError as http_e:
-        print(f"HTTP error occurred: {http_e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# print(get_api_data_json(f'https://users.roblox.com/v1/users/{post_api_data_json(f'https://users.roblox.com/v1/usernames/users', {"usernames": ['Telamon'], "excludeBannedUsers": False})["data"][0]['id']}/username-history'))
-
+    return post_api_data(url, json=json).json()
