0f951a784eb818c1c141753ac601cf95d063221d
Kottaavec2t <mathieu2008blanche@gmail.com>
2026-07-01 14:25:42 +0200

    feat: move exceptions file in templates

--- templates/exceptions.py
+++ templates/exceptions.py
@@ -1,13 +1,15 @@
 class MessageException(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         self.message = message
         super().__init__(self.message)
-    
+
     def __str__(self):
         return self.message
 
 class PlayerNotFoundException(MessageException):
-    pass
+    def __init__(self, player: str):
+        self.message = f'User **{player}** not found'
+        super().__init__(self.message)
 
 class InvalidInputException(MessageException):
     pass
